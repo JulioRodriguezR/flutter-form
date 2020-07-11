f6062097c98fbcab422fd480b89fd14900e50567
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-07-12 00:01:20 +0200

    Refactor stree proyect

---
+++ lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/myCustomForm.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
