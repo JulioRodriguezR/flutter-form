08b4e235325c1ba3a2438210d5f2004a4ff1ab85
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2020-07-14 15:16:48 +0200

    Remove many files and move code to main original

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/myCustomForm.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +9,6 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: appTitle,
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text(appTitle),
@@ -19,4 +17,74 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          allTextFieldWithMargin(),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Processing Data'),
+                    ),
+                  );
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget allTextFieldWithMargin() {
+  return Container(
+    margin: new EdgeInsets.all(20.0),
+    child: Column(
+      children: <Widget>[
+        new TextFormField(
+          initialValue: 'Hello World',
+          decoration: new InputDecoration(hintText: 'Enter Something'),
+          keyboardType: TextInputType.phone,
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Please enter some text';
+            }
+            return null;
+          },
+        ),
+      ],
+    ),
+  );
 }
